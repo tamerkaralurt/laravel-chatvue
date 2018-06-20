2e996693827168829108cc46e621a787baceafb0
Tamer <tamerkaralurt@gmail.com>
2018-06-20 05:41:56 +0300

    first commit

---
+++ app/User.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+  use Notifiable;
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'name', 'email', 'password',
+  ];
+
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+    'password', 'remember_token',
+  ];
+
+  public function friendOfMine()
+  {
+    return $this->belongsToMany('App\User', 'friends','user_id', 'friend_id');
+  }
+
+  public function friendOf()
+  {
+    return $this->belongsToMany('App\User', 'friends','friend_id', 'user_id');
+  }
+
+  public function friends()
+  {
+    return $this->friendOfMine->merge($this->friendOf);
+  }
+
+}
---
+++ resources/assets/js/components/OnlineUser.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="is-pulled-right">
+    <i class="fa fa-circle green" v-if="checkUser()"></i>
+    <i class="fa fa-circle red" v-else></i>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "OnlineUser",
+    props: ['onlineusers', 'friend'],
+    methods: {
+      checkUser: function () {
+        return _.find(this.onlineusers, {id: this.friend.id});
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .red{
+    color: red;
+  }
+  .green{
+    color: green;
+  }
+</style>
